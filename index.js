dfa01b6ca727634fceb2c8c3d6807da923ebfebd
Adam Chlebek <CHLEBEAD@mail.gvsu.edu>
2019-12-03 13:13:08 -0500

    Finalized All Node Backend

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ const server = app.listen(process.env.PORT || 4200, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
+
+app.get('/plans', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/plans.html'));
+});
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/gallery.html'));
+});
+
+app.get('/amenities', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/amenities.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/contact.html'));
+});
